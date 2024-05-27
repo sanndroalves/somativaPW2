51b01a0a71025e8040a3f7ecfe9ee1116f08312e
Alessandro Alves <sanndroalves10@hotmail.com>
2024-05-26 21:37:57 -0300

    Att settings

--- BACKEND/seraivaSomativa/settings.py
+++ BACKEND/seraivaSomativa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i171vjecr94!i2geye95l66tn#opr_=de*d*961ab1zidc02ue
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True

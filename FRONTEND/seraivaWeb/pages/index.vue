6785b9964f04b9fc9c24b6c80cfa5589c62e5693
Alessandro Alves <sanndroalves10@hotmail.com>
2024-05-28 21:30:46 -0300

    Add iframe chatbot

--- FRONTEND/seraivaWeb/pages/index.vue
+++ FRONTEND/seraivaWeb/pages/index.vue
@@ -16,13 +16,16 @@
         </Card>
 
         <Card style="width: 25rem; overflow: hidden; ">
-            <template #header>
-                <img alt="user header" src="https://lojasaraivanew.vtexassets.com/assets/vtex.file-manager-graphql/images/7f947d3e-d84a-4140-a947-23cd82ad3c6c___f24db8afb7e0b2bcd70b40c8056848a0.png" />
+            <template #title>Chat-Bot</template>
+            <template #content>
+                         <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/0f13729e-4df6-4864-98f8-586fc569160a"></iframe>           
+   
             </template>
-            <template #title>Promoção dias das Crianças</template>
+
+
         </Card>
-    
-    
-    </div>
+
+        <!-- <iframe width="350" height="430" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/0f13729e-4df6-4864-98f8-586fc569160a"></iframe>            -->
+        </div>
 </template>
 
